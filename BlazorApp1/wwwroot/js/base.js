15b6bf90bbf95d06c50704848d71f07ec9dfcbd9
DawidWik <dawidnfs@gmail.com>
2021-12-08 20:50:39 +0100

    init

---
+++ BlazorApp1/wwwroot/js/base.js
@@ -0,0 +1,27 @@
+﻿(function () {
+    const second = 1000,
+        minute = second * 60,
+        hour = minute * 60,
+        day = hour * 24;
+
+    var weddingday = "08/24/2023";
+
+    const countDown = new Date(weddingday).getTime(),
+        x = setInterval(function () {
+
+            const now = new Date().getTime(),
+                distance = countDown - now;
+
+            document.getElementById("days").innerText = Math.floor(distance / (day)),
+                document.getElementById("hours").innerText = Math.floor((distance % (day)) / (hour)).toString().padStart(2, "0"),
+                document.getElementById("minutes").innerText = Math.floor((distance % (hour)) / (minute)).toString().padStart(2, "0"),
+                document.getElementById("seconds").innerText = Math.floor((distance % (minute)) / second).toString().padStart(2, "0");
+
+            if (distance < 0) {
+                document.getElementById("headline").innerText = "To już dzisiaj!";
+                document.getElementById("countdown").style.display = "none";
+                document.getElementById("content").style.display = "block";
+                clearInterval(x);
+            }
+        }, 0)
+}());
